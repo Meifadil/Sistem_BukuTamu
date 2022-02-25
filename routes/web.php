b472815fbf18fc41a8df2286d4263c2a0d339fa9
meinurfatdilah <meifadil45@gmail.com>
2022-02-25 14:08:29 -0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+Route::get('/beranda', function () {
+    return view('admin/beranda');
+});
+
+Route::get('/data-tamu', function () {
+    return view('admin/data-tamu');
+});
+
+Route::get('/broadcast', function () {
+    return view('admin/broadcast');
+});
+
+Route::get('/landing-page', function () {
+    return view('tamu/app');
+});
+
